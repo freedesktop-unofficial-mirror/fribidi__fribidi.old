7e7f99d06716c87d41f202c49f3e6553b7a83c00
behdad <behdad>
2002-07-22 19:38:55 +0000

    Small change.

--- fribidi.c
+++ fribidi.c
@@ -1410,7 +1410,7 @@ fribidi_log2vis_get_embedding_levels (	/* input */
 
 const char *fribidi_version_info =
   FRIBIDI_PACKAGE " " FRIBIDI_VERSION "\n"
-  "interface version " FRIBIDI_INTERFACE_VERSION_STR "\n"
+  "interface version " TOSTR(FRIBIDI_INTERFACE_VERSION) "\n"
   "Unicode version " FRIBIDI_UNICODE_VERSION "\n"
   "\n"
   "Copyright (C) 2002 FriBidi Project (http://fribidi.sf.net/).\n"
